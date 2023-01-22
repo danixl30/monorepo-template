c261143225ee6580226dfc54ac612c3db765a65c
Daniel Fuentes <titofuentes1000@gmail.com>
2023-01-22 14:26:22 -0400

    feat(Root): Adding env files support
    BREAKING CHANGES:
    
    For now none

--- @app/api/src/core/infraestructure/application-module/app.module.ts
+++ @app/api/src/core/infraestructure/application-module/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ControllersModule } from '../controllers/controllers.module'
 import { DatabaseConnectionModule } from '../database/database.connection.module'
+import { EnvModule } from '../env/env.module'
 import { EventHandlerModule } from '../event-handler/event.handler.module'
 import { EventListenerModule } from '../event-listener/event.listener.module'
 import { GatewayModule } from '../gateway/gateway.module'
@@ -8,6 +9,7 @@ import { RateLimitModule } from '../rate-limit/rate.limit.module'
 
 @Module({
     imports: [
+        EnvModule,
         RateLimitModule,
         DatabaseConnectionModule,
         ControllersModule,
