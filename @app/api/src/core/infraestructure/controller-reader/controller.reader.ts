acda62158bca9f6f7a285276da1f7f44797f444a
Daniel Fuentes <titofuentes1000@gmail.com>
2023-04-04 12:37:07 -0400

    feat(Root): reflexion
    Adding reflexion logic for controllers

---
+++ @app/api/src/core/infraestructure/controller-reader/controller.reader.ts
@@ -0,0 +1,13 @@
+import { glob } from 'glob'
+import { join } from 'node:path'
+import { objectValues } from '@mono/object-utils'
+
+export const initializeControllers = (currentPath: string) => {
+    const data = glob.sync(
+        join(currentPath, '../controllers/*.controller.js').replace(/\\/g, '/'),
+    )
+    return data.map((e) => {
+        const module = require(e)
+        return objectValues(module)[0]
+    })
+}
