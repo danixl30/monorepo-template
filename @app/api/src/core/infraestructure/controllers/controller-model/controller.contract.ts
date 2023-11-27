2f8ab281234c3f746d452b560dc766aab645dcf5
Daniel Fuentes <titofuentes1000@gmail.com>
2023-11-26 21:09:01 -0400

    feat(Root): types
    Improving types in controllers

--- @app/api/src/core/infraestructure/controllers/controller-model/controller.contract.ts
+++ @app/api/src/core/infraestructure/controllers/controller-model/controller.contract.ts
@@ -1,3 +1,5 @@
-export interface ControllerContract<T extends any[], U> {
-    execute(...args: T): Promise<U>
+export type UnwrapInput<T> = T extends any[] ? T : T[]
+
+export interface ControllerContract<T, U> {
+    execute(...args: UnwrapInput<T>): Promise<U>
 }
