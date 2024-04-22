22f2e653f52230c0ea89019d3ef180fc01b66254
Daniel Fuentes <titofuentes1000@gmail.com>
2024-04-22 14:37:53 -0400

    feat(Root): test
    Improving expectation contract in tests

--- @app/api/tests/unit/suits/example/tests/second-test.test.ts
+++ @app/api/tests/unit/suits/example/tests/second-test.test.ts
@@ -1,4 +1,9 @@
 export const name = 'second test'
 export const body = () => {
+    lookFor({
+        a: '',
+    }).toMathObject({
+        a: '',
+    })
     lookFor(2 + 2).equals(4)
 }
--- packages/test-utils/lib/contracts/expectation.contract.ts
+++ packages/test-utils/lib/contracts/expectation.contract.ts
@@ -1,32 +1,58 @@
-export type ExpectationContract = <T>(value: T) => {
+export type ExpectationContract = <T = any>(
+    value: T,
+) => {
     equals(value: T): void
     notEquals(value: T): void
-    toHaveLenght: (number: number) => void
-    toNotHaveLenght: (number: number) => void
-    toHavePropety: (path: string | string[], valueInPath?: any) => void
-    toNotHavePropety: (path: string | string[], valueInPath?: any) => void
-    toCloseTo: (number: number) => void
+    toHaveLenght: T extends any[] | object ? (number: number) => void : never
+    toNotHaveLenght: T extends any[] | object ? (number: number) => void : never
+    toHavePropety: T extends object
+        ? (path: string | string[], valueInPath?: any) => void
+        : never
+    toNotHavePropety: T extends object
+        ? (path: string | string[], valueInPath?: any) => void
+        : never
+    toCloseTo: T extends number ? (number: number) => void : never
     toBeDefined: () => void
     toBeFalsy: () => void
-    toBeGreaterThan: (number: number | bigint) => void
-    toNotBeGreaterThan: (number: number | bigint) => void
-    toBeGreaterThanOrEqual: (number: number | bigint) => void
-    toNotBeGreaterThanOrEqual: (number: number | bigint) => void
-    toBeLessThan: (number: number | bigint) => void
-    toNotBeLessThan: (number: number | bigint) => void
-    toBeLessThanOrEqual: (number: number | bigint) => void
-    toNotBeLessThanOrEqual: (number: number | bigint) => void
+    toBeGreaterThan: T extends number | bigint
+        ? (number: number | bigint) => void
+        : never
+    toNotBeGreaterThan: T extends number | bigint
+        ? (number: number | bigint) => void
+        : never
+    toBeGreaterThanOrEqual: T extends number | bigint
+        ? (number: number | bigint) => void
+        : never
+    toNotBeGreaterThanOrEqual: T extends number | bigint
+        ? (number: number | bigint) => void
+        : never
+    toBeLessThan: T extends number | bigint
+        ? (number: number | bigint) => void
+        : never
+    toNotBeLessThan: T extends number | bigint
+        ? (number: number | bigint) => void
+        : never
+    toBeLessThanOrEqual: T extends number | bigint
+        ? (number: number | bigint) => void
+        : never
+    toNotBeLessThanOrEqual: T extends number | bigint
+        ? (number: number | bigint) => void
+        : never
     toBeNull: () => void
     toNotBeNull: () => void
     toBeTruthy: () => void
     toBeUndefined: () => void
     toBeNaN: () => void
     toNotBeNaN: () => void
-    toDeepEqual: (valueToCompare: object) => void
-    toMatch: (regExp: string | RegExp) => void
-    toNotMatch: (regExp: string | RegExp) => void
-    toMathObject: (valueToCompare: object) => void
+    toDeepEqual: T extends object ? (valueToCompare: object) => void : never
+    toMatch: T extends string ? (regExp: string | RegExp) => void : never
+    toNotMatch: T extends string ? (regExp: string | RegExp) => void : never
+    toMathObject: T extends object ? (valueToCompare: object) => void : never
     toBeError: (error?: Error | string) => void
-    asyncResolve(manager?: (value: Awaited<T>) => void): Promise<void>
-    asyncReject<U = unknown>(manager?: (err: U) => void): Promise<void>
+    asyncResolve: T extends Promise<any>
+        ? (manager?: (value: Awaited<T>) => void) => Promise<void>
+        : never
+    asyncReject: T extends Promise<any>
+        ? <U = unknown>(manager?: (err: U) => void) => Promise<void>
+        : never
 }
