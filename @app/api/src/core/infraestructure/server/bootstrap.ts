f83eceda04bd4ca1e6fc3fcdcdce153ea0d44907
Daniel Fuentes <titofuentes1000@gmail.com>
2024-09-28 10:15:00 -0400

    fix(Root): dependencies
    Updating dependencies to latest version

--- @app/api/src/core/infraestructure/server/bootstrap.ts
+++ @app/api/src/core/infraestructure/server/bootstrap.ts
@@ -6,13 +6,12 @@ import { runServer } from './run/run.server'
 export default async function bootstrap() {
 	const builder = await ServerBuilder.create(AppModule)
 	const app = builder.setCors().setGlobalProfix('api').setValidationPipe()
-	if (process.env.NODE_ENV === 'development')
-		app.setDocumentation({
-			title: 'Gymnastic center API Docs',
-			description: 'API Documentation for Gymnastic center',
-			version: '1.0',
-			path: 'api/docs',
-			bearerAuth: true,
-		})
+	app.setDocumentation({
+		title: 'Gymnastic center API Docs',
+		description: 'API Documentation for Gymnastic center',
+		version: '1.0',
+		path: 'api/docs',
+		bearerAuth: true,
+	})
 	await runServer(app.build(), PORT)
 }
