a31d58e7fc05c2663b97098b76617e76a49fdbbf
Daniel Fuentes <titofuentes1000@gmail.com>
2024-11-01 20:39:15 -0400

    fix(Root): format
    Format code

--- @app/api/src/utils/call-stack/get.call.stack.ts
+++ @app/api/src/utils/call-stack/get.call.stack.ts
@@ -1,19 +1,19 @@
-import { getCallSite } from "node:util";
+import { getCallSite } from 'node:util'
 
 declare module 'node:util' {
-    export function getCallSite(frames?: number): {
-        fuctionName: string
-        scriptName: string
-        lineNumber: number
-        column: number
-    }[]
+	export function getCallSite(frames?: number): {
+		fuctionName: string
+		scriptName: string
+		lineNumber: number
+		column: number
+	}[]
 }
 
 export const getCallStack = (): string[] => {
-    const callSatck = getCallSite()
-    return callSatck
-        .map(e => e.scriptName)
-        .map((e) => e.replace('file:///', ''))
-        .map((e) => (process.platform !== 'win32' ? '/' + e : e))
-        .reverse()
+	const callSatck = getCallSite()
+	return callSatck
+		.map((e) => e.scriptName)
+		.map((e) => e.replace('file:///', ''))
+		.map((e) => (process.platform !== 'win32' ? '/' + e : e))
+		.reverse()
 }
