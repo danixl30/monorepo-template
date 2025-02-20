25e95dd7d834fe40b6d73709b866e56c4fa09f91
Daniel Fuentes <titofuentes1000@gmail.com>
2025-02-20 19:41:31 -0400

    refactor(Root): uuid
    Changing UUID generator

--- @app/api/src/core/infraestructure/UUID/service/concrete.UUID.generator.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common'
-import { IDGenerator } from 'src/core/application/ID/ID.generator'
-
-@Injectable()
-export class ConcreteUUIDGenerator {
-	generate(): IDGenerator<string> {
-		return () => crypto.randomUUID()
-	}
-}
