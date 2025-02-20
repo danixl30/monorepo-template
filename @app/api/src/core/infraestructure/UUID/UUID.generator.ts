c8b80c630a42d07c57639bf897be79f161a904b2
Daniel Fuentes <titofuentes1000@gmail.com>
2025-02-20 19:43:22 -0400

    fix(Root): format
    format

--- @app/api/src/core/infraestructure/UUID/UUID.generator.ts
+++ @app/api/src/core/infraestructure/UUID/UUID.generator.ts
@@ -1,4 +1,4 @@
-import { randomUUID } from "node:crypto";
-import { IDGenerator } from "src/core/application/ID/ID.generator";
+import { randomUUID } from 'node:crypto'
+import { IDGenerator } from 'src/core/application/ID/ID.generator'
 
 export const generateUUID: IDGenerator<string> = () => randomUUID()
